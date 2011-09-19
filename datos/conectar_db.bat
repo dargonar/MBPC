309d6a8b9d71d5bd97d9f1a44d0de3ae1ac29d29
matu <matias.romeo@diventi.com.ar>
2011-09-19 18:59:46 -0300

    scripts de conexion a db y web agrgan rutas ajenas

--- datos/conectar_db.bat
+++ datos/conectar_db.bat
@@ -5,7 +5,9 @@ if "%errorlevel%" NEQ "0"; goto error
 FOR /F "tokens=2 delims=:" %%a in ('IPCONFIG ^|FIND "IP" ^|FIND "192.168.41."') do set _IP=%%a
 set IP=%_IP:~1%
 echo Poniendo GW para IPs en %IP%
+
 route add 192.168.10.0 mask 255.255.255.0 %IP%
+route add 192.168.50.0 mask 255.255.255.0 %IP%
 
 ping -t 192.168.10.120
 goto fin
